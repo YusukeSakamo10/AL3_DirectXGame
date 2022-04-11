4748114bd23deccb7bb8641d0d52af11951547d4
YusukeSakamoto <k021g1487@g.neec.ac.jp>
2022-04-11 17:52:56 +0900

    やり直し

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("GE2A_05_サカモト_ユウスケ_AL3");
+	win->CreateGameWindow("GE2A_05_サカモト_ユウスケ_");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
